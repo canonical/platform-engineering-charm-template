40f3749cce62a44e4bae8477ffb3f62f5be301e5
arturo-seijas <arturo.seijas@canonical.com>
2023-09-07 17:03:47 +0200

    Fix integration test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -20,7 +20,7 @@ APP_NAME = METADATA["name"]
 
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest):
+async def test_build_and_deploy(ops_test: OpsTest, pytestconfig: pytest.Config):
     """Build the charm-under-test and deploy it together with related charms.
 
     Assert on the unit status before any relations/configurations take place.
@@ -30,9 +30,10 @@ async def test_build_and_deploy(ops_test: OpsTest):
     resources = {"httpbin-image": METADATA["resources"]["httpbin-image"]["upstream-source"]}
 
     # Deploy the charm and wait for active/idle status
+    charm = pytestconfig.getoption("--charm-file")
     assert ops_test.model
     await asyncio.gather(
-        ops_test.model.deploy(charm, resources=resources, application_name=APP_NAME),
+        ops_test.model.deploy(f"./{charm}", resources=resources, application_name=APP_NAME),
         ops_test.model.wait_for_idle(
             apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
         ),
