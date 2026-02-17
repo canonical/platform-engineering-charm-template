8ea04dc04e6f8dec56fb46e05a63b6296a8fc2f0
Sebastien Georget <sebastien.georget@canonical.com>
2026-02-17 16:14:48 +0100

    Simplify tests

--- terraform/tests/main.tf
+++ terraform/tests/main.tf
@@ -1,18 +1,6 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
-variable "channel" {
-  description = "The channel to use when deploying a charm."
-  type        = string
-  default     = "latest/edge"
-}
-
-variable "revision" {
-  description = "Revision number of the charm."
-  type        = number
-  default     = null
-}
-
 terraform {
   required_providers {
     juju = {
@@ -24,10 +12,17 @@ terraform {
 
 provider "juju" {}
 
+resource "juju_model" "testing" {
+  name = "tf-testing-${formatdate("YYYYMMDDhhmmss", timestamp())}"
+}
+
 module "__charm_name__" {
-  source   = "./.."
+  source     = "./.."
+  model_uuid = juju_model.testing.uuid
+
   app_name = "__charm_name__"
-  channel  = var.channel
-  model    = "prod-__charm_name__-example"
-  revision = var.revision
+  channel  = "latest/edge"
+
+  # renovate: depName="__charm_name__"
+  revision = 1
 }
