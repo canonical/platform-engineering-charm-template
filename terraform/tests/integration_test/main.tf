a8b8f309206da3b1a4fc6b1940a26f27c824d5ca
Sebastien Georget <sebastien.georget@canonical.com>
2026-02-18 13:34:39 +0100

    Enrich tests

---
+++ terraform/tests/integration_test/main.tf
@@ -0,0 +1,29 @@
+# Copyright 2025 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+terraform {
+  required_version = ">= 1.0"
+  required_providers {
+    juju = {
+      version = "> 1.1.0"
+      source  = "juju/juju"
+    }
+  }
+}
+
+provider "juju" {}
+
+variable "model_uuid" {
+  type = string
+}
+
+# Add some specific tests here, like integrations
+
+# tflint-ignore: terraform_unused_declarations
+data "external" "app_status" {
+  program = ["bash", "${path.module}/wait-for-active.sh", var.model_uuid, "__charm_name__"]
+
+  #depends_on = [
+  #  juju_integration....
+  #]
+}
--- terraform/tests/main.tftest.hcl
+++ terraform/tests/main.tftest.hcl
@@ -1,9 +1,22 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+run "setup_tests" {
+  module {
+    source = "./tests/setup"
+  }
+}
+
 run "basic_deploy" {
+  variables {
+    model_uuid = run.setup_tests.model_uuid
+    channel    = "latest/edge"
+    # renovate: depName="__charm_name__"
+    revision = 1
+  }
+
   assert {
-    condition     = module.__charm_name__.app_name == "__charm_name__"
+    condition     = output.app_name == "__charm_name__"
     error_message = "__charm_name__ app_name did not match expected"
   }
 }
